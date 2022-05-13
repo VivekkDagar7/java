d6ef33bb20af15a11a81fa66fddc11b16d81cd6e
VivekkDagar7 <92029657+VivekkDagar7@users.noreply.github.com>
2022-05-13 13:36:01 +0530

    Demonstrating the use of char and boolean datatype

---
+++ CharAndBoolean/src/j/learnprogramming/Main.java
@@ -0,0 +1,16 @@
+package j.learnprogramming;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        char myChar = 'T';
+        char myUnicodeChar = '\u0054';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+    }
+}
